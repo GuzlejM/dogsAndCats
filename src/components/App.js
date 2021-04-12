9bbf79b67f75f23d7c627fff8bb85f955c8ca1cf
guzlej <martin.guzlej@gmail.com>
2021-04-12 10:02:52 +0200

    Basic compoennts created

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Categories from './Categories';
+import List from './List';
+
+
+const App = () => {    
+    return (
+    <div>
+        <h1>
+            APP
+        </h1>
+        <Navbar />
+        <Categories />
+        <List />
+    </div>
+  );
+};
+
+export default App

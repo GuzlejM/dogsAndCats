a364e0a9efb77d790f0a10234f85a6e1c1de565e
guzlej <martin.guzlej@gmail.com>
2021-04-14 11:39:42 +0200

    listing hardcoded "dogs

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,41 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Navbar from './Navbar';
 import Categories from './Categories';
-import List from './List';
+//import List from './List';
 import Footer from './Footer';
 import unsplash from '../apis/unsplash';
 
 
 const App = () => {
+  const [ results, setResults ] = useState([]);
+
+
   const clientId = 'YGGMc3qjq5OLyki5BXQAKCV9ZMkJd59c7LRUI6kVlb0';
+
   const getResponse = async () => {
-    const res = await axios.get(`https://api.unsplash.com/search/photos?query=dogs&client_id=${clientId}`)
-    console.log(res.data.results)
-    return res
+    const res = await axios.get(
+      `https://api.unsplash.com/search/photos?query=dog&client_id=${clientId}&count=10`
+      )
+    setResults(res.data.results)
+    console.log(res)
   }
 
-  getResponse();
+  getResponse()
 
     return (
     <div className='p-2'>
         <Navbar />
         <Categories />
-        
-        <List />
+          {results.map((img) => {
+            return (
+              <div key={img.id}>
+                <img 
+                src={img.urls.small}
+                alt="unsplashImg"
+                className='w-full h-auto rounded-lg mt-4'/>
+              </div>
+            )
+          })}
         <Footer />
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {    
     return (
-    <div>
+    <div className='mt-4'>
         <hr></hr>
         <footer className="text-gray-600 body-font">
           <div className="container px-5 py-2 mx-auto flex items-center sm:flex-row flex-col">

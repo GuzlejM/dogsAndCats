2325ff1fa883ad55686dbb59753082fa0589af20
guzlej <martin.guzlej@gmail.com>
2021-04-14 08:47:56 +0200

    categories started

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Categories = () => {    
     return (
     <div>
-        <h1>
-            Categories
-        </h1>
+        <button className="w-1/3">Dogs</button>
+        <button className="w-1/3">Cats</button>
+        <button className="w-1/3">Other</button>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <li className='w-4/5'>Dogs and Cats</li>
             <li className='flex w-1/5'>
                 <div className='w-1/2'><i className="far fa-bell"></i></div>
-                <div className='w-1/2'><i class="far fa-user"></i></div>
+                <button className='w-1/2'><i class="far fa-user"></i></button>
             </li>
 
         </ul>
